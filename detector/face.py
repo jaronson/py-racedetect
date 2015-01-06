d9cf994071ceb0f596966139643f39b1dfad36fa
Josh Aronson <jparonson@gmail.com>
2015-01-06 13:03:45 -0600

    Add working face detector

--- detector/face.py
+++ detector/face.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv
 import cv2
 
 from collections import namedtuple
@@ -8,30 +9,19 @@ def create_cascades(**kwargs):
     return namedtuple('CascadeSet', kwargs.keys())(*files)
 
 class Face(object):
-    cascades = create_cascades(face='haar/frontalface_default')
+    def __init__(self):
+        self.cascades = create_cascades(face='haar/frontalface_default')
 
-    def cascades(self):
-        return self.__class__.cascades
-
-class Eye(Face):
-    cascades = create_cascades(face='haar/frontalface_default', eye='haar/eye')
-
-    def detect(self, imagepath):
-        c = cv2.createLBPHFaceRecognizer()
-        return 0
-        image = cv2.imread(imagepath)
+    def detect(self, frame):
+        image = np.asarray(frame[:,:])
         gray  = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         faces = self.cascades.face.detectMultiScale(gray, 1.3, 5)
 
         for (x,y,w,h) in faces:
-            cv2.rectangle(image ,(x,y), (x+w,y+h), (255,0,0), 2)
-            roi_gray  = gray[y:y+h, x:x+w]
-            roi_color = image[y:y+h, x:x+w]
-            eyes      = self.cascades.eye.detectMultiScale(roi_gray)
+            cv2.rectangle(image, (x,y), (x+w, y+h), (255,0,0), 2)
 
-            for (ex,ey,ew,eh) in eyes:
-                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-        cv2.imshow('image',image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        bitmap = cv.CreateImageHeader((image.shape[1], image.shape[0]), cv.IPL_DEPTH_8U, 3)
+        cv.SetData(bitmap, image.tostring(), image.dtype.itemsize * 3 * image.shape[1])
+        cv.ShowImage('w1', bitmap)
+        cv.WaitKey(0)
+        cv.DestroyAllWindows()
