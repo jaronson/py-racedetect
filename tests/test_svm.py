c9964f9002ff3a3ad4899c98922bbd5b1147dbe3
Josh Aronson <jparonson@gmail.com>
2015-03-10 18:59:15 -0500

    Add svm.FeretPerson, FeretImage, tests

--- tests/test_svm.py
+++ tests/test_svm.py
@@ -1,9 +1,29 @@
 import unittest
+import simplejson as json
 from racedetect import svm
 
-class TestTrainingAsset(unittest.TestCase):
+class TestFeretPerson(unittest.TestCase):
     def setUp(self):
-        self.asset = svm.TrainingAsset()
+        self.path   = 'tests/data/00001'
+        self.truths = json.load(open('tests/data/00001/truths.json'))
+        self.person = svm.FeretPerson()
 
     def test_load(self):
-        svm.load()
+        self.person.load(self.path)
+        self.assertEqual(self.person.label, '00001')
+        self.assertEqual(self.person.id, self.truths['id'])
+        self.assertEqual(self.person.gender, self.truths['gender'])
+        self.assertEqual(self.person.race, self.truths['race'])
+        self.assertEqual(self.person.yob, self.truths['yob'])
+
+class TestFeretImage(unittest.TestCase):
+    def setUp(self):
+        self.path   = 'tests/data/00001'
+        self.person = svm.FeretPerson()
+        self.person.load(self.path)
+
+        self.image = self.person.images[0]
+
+    def test_init(self):
+        self.assertEqual(self.image.person_label, '00001')
+        self.assertEqual(self.image.person_age, 50)
