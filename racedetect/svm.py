f6cc7031a3c649ce877b35afbaf62c0de8bfdbc4
Josh Aronson <jparonson@gmail.com>
2015-03-10 15:04:54 -0500

    Add svm module

---
+++ racedetect/svm.py
@@ -0,0 +1,11 @@
+import os
+import glob
+import cv2
+import cv2.cv as cv
+import utils
+import config
+import numpy as np
+import sklearn.svm as sk_svm
+
+class RaceSvm(object):
+    pass
---
+++ tests/test_svm.py
@@ -0,0 +1,5 @@
+import unittest
+from racedetect import svm
+
+class TestSvm(unittest.TestCase):
+    pass
