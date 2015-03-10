b683ba08859c0c3153fb482f2dceb3d069a56a70
Josh Aronson <jparonson@gmail.com>
2015-03-10 17:40:50 -0500

    Add test data

--- tests/test_svm.py
+++ tests/test_svm.py
@@ -1,5 +1,9 @@
 import unittest
 from racedetect import svm
 
-class TestSvm(unittest.TestCase):
-    pass
+class TestTrainingAsset(unittest.TestCase):
+    def setUp(self):
+        self.asset = svm.TrainingAsset()
+
+    def test_load(self):
+        svm.load()
